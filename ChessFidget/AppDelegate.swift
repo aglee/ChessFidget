cbe4f59caf856598d355ef996f605928daa21dfb
Andy Lee <aglee@mac.com>
2016-08-24 12:54:43 -0400

    Initial Commit

---
+++ ChessFidget/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ChessFidget
+//
+//  Created by Andy Lee on 8/24/16.
+//  Copyright © 2016 Andy Lee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
