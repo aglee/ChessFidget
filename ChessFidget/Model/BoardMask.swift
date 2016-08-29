a27a9ba495cd241b9d3f31a722b68842298db81a
Andy Lee <aglee@mac.com>
2016-08-29 15:04:08 -0400

    Added class BoardMask.

---
+++ ChessFidget/Model/BoardMask.swift
@@ -0,0 +1,14 @@
+//
+//  BoardMask.swift
+//  ChessFidget
+//
+//  Created by Andy Lee on 8/29/16.
+//  Copyright © 2016 Andy Lee. All rights reserved.
+//
+
+class BoardMask: Grid64<Bool> {
+	init() {
+		super.init(value: false)
+	}
+}
+
