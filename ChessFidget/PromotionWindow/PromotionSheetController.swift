ca4474020350ffc0542f994c74f8513ca46d2a1c
Andy Lee <aglee@mac.com>
2016-09-06 12:15:45 -0400

    Starting to add UI to select what piece to promote to.

---
+++ ChessFidget/PromotionWindow/PromotionSheetController.swift
@@ -0,0 +1,19 @@
+//
+//  PromotionSheetController.swift
+//  ChessFidget
+//
+//  Created by Andy Lee on 9/6/16.
+//  Copyright © 2016 Andy Lee. All rights reserved.
+//
+
+import Cocoa
+
+class PromotionSheetController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
