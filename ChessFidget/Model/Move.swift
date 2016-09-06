6546b76efbbb84cd18c50bc191d1ac3dc76b9e32
Andy Lee <aglee@mac.com>
2016-09-06 12:27:53 -0400

    Gave PromotionType an Int raw value.  Intent is to use the raw values as tags on radio buttons.

--- ChessFidget/Model/Move.swift
+++ ChessFidget/Model/Move.swift
@@ -6,19 +6,19 @@
 //  Copyright © 2016 Andy Lee. All rights reserved.
 //
 
-enum PromotionType {
-	case promoteToKnight
-	case promoteToBishop
+enum PromotionType: Int {
+	case promoteToQueen = 0
 	case promoteToRook
-	case promoteToQueen
+	case promoteToBishop
+	case promoteToKnight
 
 	var pieceType: PieceType {
 		get {
 			switch self {
-			case .promoteToKnight: return .Knight
-			case .promoteToBishop: return .Bishop
-			case .promoteToRook: return .Rook
 			case .promoteToQueen: return .Queen
+			case .promoteToRook: return .Rook
+			case .promoteToBishop: return .Bishop
+			case .promoteToKnight: return .Knight
 			}
 		}
 	}
