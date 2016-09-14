d44eb499cf33fea243bd9c75a4a5e810f9790fd4
Andy Lee <aglee@mac.com>
2016-09-14 13:31:14 -0400

    Added GridPointXY.init?(algebraic:).

--- ChessFidget/Utilities/GridPointXY.swift
+++ ChessFidget/Utilities/GridPointXY.swift
@@ -21,6 +21,16 @@ struct GridPointXY: Equatable, Hashable {
 		self.y = y
 	}
 
+	/// Returns nil if the string is not a valid two-character square notation (uppercase okay).
+	init?(algebraic: String) {
+		guard let xyPair = GridPointXY.xyFromString(algebraic: algebraic) else {
+			return nil
+		}
+
+		x = xyPair.x
+		y = xyPair.y
+	}
+
 	// MARK: - Hashable protocol
 
 	var hashValue: Int {
@@ -31,6 +41,33 @@ struct GridPointXY: Equatable, Hashable {
 
 	public static func ==(lhs: GridPointXY, rhs: GridPointXY) -> Bool {
 		return lhs.x == rhs.x && lhs.y == rhs.y
+	}
+
+	// MARK: - Private methods
+
+	private static func xyFromString(algebraic: String) -> (x: Int, y: Int)? {
+		let unichars = Array(algebraic.lowercased().unicodeScalars)
+
+		if unichars.count != 2 {
+			print("ERROR: Cannot convert '\(algebraic)' - length must be 2.")
+			return nil
+		}
+
+		let fileChar = unichars[0]
+		let rankChar = unichars[1]
+
+		if fileChar < "a" || fileChar > "h" {
+			print("ERROR: Cannot convert '\(algebraic)' - file character must be in 'a'...'h'.")
+			return nil
+		}
+
+		if rankChar < "1" || rankChar > "8" {
+			print("ERROR: Cannot convert '\(algebraic)' - rank character must be in '1'...'8'.")
+			return nil
+		}
+
+		return (x: Int(fileChar.value - UnicodeScalar("a").value),
+		        y: Int(rankChar.value - UnicodeScalar("1").value))
 	}
 }
 
