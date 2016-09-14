343e1ddccd855461bf7daca1d2ec0e390516334f
Andy Lee <aglee@mac.com>
2016-09-14 11:41:15 -0400

    Tweaked QuietLog to not add a newline if the output string already ends with one.

--- ChessFidget/Chess Engine/QuietLog.mm
+++ ChessFidget/Chess Engine/QuietLog.mm
@@ -13,7 +13,11 @@ void QuietLog(NSString *format, ...)
 	va_start(argList, format);
 	{{
 		NSString *message = [[NSString alloc] initWithFormat:format arguments:argList];
-		fprintf(stderr, "%s\n", message.UTF8String);
+		if ([message hasSuffix:@"\n"]) {
+			fprintf(stderr, "+++ %s", message.UTF8String);
+		} else {
+			fprintf(stderr, "+++ %s\n", message.UTF8String);
+		}
 	}}
 	va_end(argList);
 }
