f64bfe149f12381c794357f41e71d3c0fe697215
Andy Lee <aglee@mac.com>
2016-09-19 23:56:48 -0400

    Moved two methods from GridPointXY.swift to Move.swift, since they're chess-specific.

--- ChessFidget/Model/Move.swift
+++ ChessFidget/Model/Move.swift
@@ -57,6 +57,16 @@ extension GridPointXY {
 	private static let fileCharacters: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h"]
 	private static let rankCharacters: [Character] = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
+	/// Returns nil if the string is not a valid two-character square notation (uppercase okay).
+	init?(algebraic: String) {
+		guard let xyPair = GridPointXY.xyFromString(algebraic: algebraic) else {
+			return nil
+		}
+
+		x = xyPair.x
+		y = xyPair.y
+	}
+	
 	// Converts the point to algebraic chess notation, where (0,0) is "a1" and (7,7) is h8.
 	var squareName: String {
 		if x == x % GridPointXY.fileCharacters.count
@@ -65,6 +75,33 @@ extension GridPointXY {
 		} else {
 			return "GridPointXY(\(x),\(y))"
 		}
+	}
+
+	// MARK: - Private methods
+
+	private static func xyFromString(algebraic: String) -> (x: Int, y: Int)? {
+		let unichars = Array(algebraic.lowercased().unicodeScalars)
+
+		if unichars.count != 2 {
+			print("ERROR: Cannot convert '\(algebraic)' - length must be 2.")
+			return nil
+		}
+
+		let fileChar = unichars[0]
+		let rankChar = unichars[1]
+
+		if fileChar < "a" || fileChar > "h" {
+			print("ERROR: Cannot convert '\(algebraic)' - file character must be in 'a'...'h'.")
+			return nil
+		}
+
+		if rankChar < "1" || rankChar > "8" {
+			print("ERROR: Cannot convert '\(algebraic)' - rank character must be in '1'...'8'.")
+			return nil
+		}
+
+		return (x: Int(fileChar.value - UnicodeScalar("a").value),
+		        y: Int(rankChar.value - UnicodeScalar("1").value))
 	}
 
 }
