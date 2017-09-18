b2b58cf96aa1fd6da3100717139f74f00de6623d
Andy Lee <aglee@mac.com>
2017-09-18 07:25:20 -0400

    Added moveFromAlgebraicString() to Position.
    Will be removing moveStringForEngine() from Game.

--- ChessFidget/Model/Position.swift
+++ ChessFidget/Model/Position.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Andy Lee. All rights reserved.
 //
 
+import Foundation
+
 /// Represents the state of a chess game, including all information needed to determine whether any proposed move is legal.  The default initializer sets up the board with all pieces on their home squares and White to move.
 struct Position {
 	var board = Board()
@@ -54,6 +56,62 @@ struct Position {
 
 		// It's the other player's turn now.
 		whoseTurn = whoseTurn.opponent
+	}
+
+	/// Converts a string like "e2e4" or "a7a8q" into a Move instance.  If the
+	/// input string does not represent a valid move, returns nil.
+	func moveFromAlgebraicString(_ algebraic: String, reportErrors: Bool) -> Move? {
+		var str = algebraic.lowercased() as NSString
+		if str.hasSuffix("\n") {
+			str = str.substring(to: str.length - 1) as NSString
+		}
+
+		guard str.length == 4 || str.length == 5 else {
+			if reportErrors {
+				print("ERROR: Engine string '\(str)' has unexpected length.")
+			}
+			return nil
+		}
+
+		let startPointString = str.substring(with: NSMakeRange(0, 2))
+		guard let startPoint = GridPointXY(algebraic: startPointString) else {
+			if reportErrors {
+				print("ERROR: Engine string '\(str)' has invalid start square.")
+			}
+			return nil
+		}
+
+		let endPointString = str.substring(with: NSMakeRange(2, 2))
+		guard let endPoint = GridPointXY(algebraic: endPointString) else {
+			if reportErrors {
+				print("ERROR: Engine string '\(str)' has invalid end square.")
+			}
+			return nil
+		}
+
+		let validity = MoveValidator(position: self,
+		                             startPoint: startPoint,
+		                             endPoint: endPoint).validateMove()
+		switch validity {
+		case .invalid(_):
+			return nil
+		case .valid(let moveType):
+			if case .pawnPromotion(_) = moveType {
+				assert(str.length == 5, "ERROR: Engine string '\(str)' represents a pawn promotion, but does not specify the piece to promote to.")
+				let promoType: PromotionType
+				switch str.substring(from: 4).lowercased() {
+				case "b": promoType = .promoteToBishop
+				case "n": promoType = .promoteToKnight
+				case "r": promoType = .promoteToRook
+				case "q": promoType = .promoteToQueen
+				default:
+					fatalError("Unexpected promotion type in move string '\(str)' received from the engine.")
+				}
+				return Move(from: startPoint, to: endPoint, type: .pawnPromotion(type: promoType))
+			} else {
+				return Move(from: startPoint, to: endPoint, type: moveType)
+			}
+		}
 	}
 }
 
