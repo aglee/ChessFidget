029f2884766907d23e51310a92364c51ba4cf662
Andy Lee <aglee@mac.com>
2017-09-18 10:00:10 -0400

    Changes again to the cases in enum GameState.

--- ChessFidget/Model/GameState.swift
+++ ChessFidget/Model/GameState.swift
@@ -6,19 +6,18 @@
 //  Copyright © 2016 Andy Lee. All rights reserved.
 //
 
-enum ReasonGameIsOver: String {
+/// Is the game over or not?
+enum GameState {
+	case awaitingMove
+	case gameIsOver(reason: ReasonGameIsOver)
+}
+
+/// Reasons a game can be over.
+enum ReasonGameIsOver: String {  // TODO: Localize.
 	case blackWinsByCheckmate = "Black wins by checkmate"
 	case whiteWinsByCheckmate = "White wins by checkmate"
 	case drawDueToStalemate = "Draw due to stalemate"
 	//TODO: Add case drawDueToInsufficientMaterial
 	//TODO: Add case drawDueTo50MoveRule
-}
-
-/// Used for keeping track of game flow and performing sanity checks (are we
-/// trying to do something we shouldn't be doing given the current game state?).
-enum GameState {
-	case awaitingGameStart
-	case awaitingMove(player: Player)
-	case gameIsOver(reason: ReasonGameIsOver)
 }
 
